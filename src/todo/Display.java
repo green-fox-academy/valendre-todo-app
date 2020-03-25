3d127770b47213a218324a0127ae5faa99cd2ecd
valendre <valendre@gmail.com>
2020-03-25 11:58:20 +0100

    first method is ready.

---
+++ src/todo/Display.java
@@ -0,0 +1,14 @@
+package todo;
+
+public class Display {
+  public static void printUsage() {
+    System.out.println("Command Line Todo application\n" +
+        "=============================\n" +
+        "\n" +
+        "Command line arguments:\n" +
+        "    -l   Lists all the tasks\n" +
+        "    -a   Adds a new task\n" +
+        "    -r   Removes an task\n" +
+        "    -c   Completes an task");
+  }
+}
--- src/todo/Todo.java
+++ src/todo/Todo.java
@@ -1,4 +1,12 @@
 package todo;
 
 public class Todo {
+  public static void main(String[] args) {
+
+    if (args.length==0) {
+      Display.printUsage();
+    }
+
+  }
+
 }
